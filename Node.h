59f1ab5cf34366122fa4073b2d2df0f2f0755253
claudia-maria-dudau <62221313+claudia-maria-dudau@users.noreply.github.com>
2020-05-07 18:58:32 +0300

    Node

---
+++ Node.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+template <typename K, typename V, typename F> class Dictionary;
+
+//clasa pentru noduri
+template <typename K, typename V>
+class Node {
+	K key;
+	V value;
+	char color;
+	Node* left, * right;
+
+public:
+	Node(K&, V&);	
+
+	friend class Dictionary<K, V, F>;
+};
+
+//constructor cu parametrii
+template <typename K, typename V>
+Node<K, V>::Node(K& k, V& v) : key(k), value(v), color("r"), left(NULL), right(NULL) {}
