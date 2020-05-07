e824ef2a40d0fd912631bda70f5e98655ba9b7d6
claudia-maria-dudau <62221313+claudia-maria-dudau@users.noreply.github.com>
2020-05-07 18:36:39 +0300

    KeyComp

---
+++ KeyComp.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+//clasa template KeyComp
+template <typename K>
+class KeyComp {
+public:
+	//supraincarcare operator () ce returneaza true daca
+	//cheia 1 este mai mica decat cheia 2 sau false altfel
+	bool operator () (const K& key1, const K& key2) {
+		if (key1 < key2)
+			return true;
+		else
+			return false;
+	}
+};
+
+//clasa specializata pentru stringuri KeyComp
+template <>
+class KeyComp<string> {
+public:
+	//compararea se face doar intre primele n/2 caractere
+	//ale fiecarei chei (n = min lungimilor celor doua chei)
+	bool operator () (const string& key1, const string& key2) {
+		int n = min(key1.length(), key2.length()) / 2;
+		if (key1.substr(0, n) > key2.substr(0, n))
+			return true;
+		else
+			return false;
+	}
+};
+
